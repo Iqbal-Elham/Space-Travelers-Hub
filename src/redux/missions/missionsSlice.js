5986a8f7fbbcaed79a3fd4d8626b488dc88b9449
Fithamlak <justiceofgod01@gmail.com>
2023-03-14 18:49:29 +0300

    Finish createSlice for Missions and feachMissions method to feach all missions from api

---
+++ src/redux/missions/missionsSlice.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const missionApiUrl = 'https://api.spacexdata.com/v3/missions';
+
+const initialState = {
+  missions: {},
+  ifSucceed: false,
+  isLoading: false,
+  error: null,
+};
+
+const fetchMissions = createAsyncThunk(
+  'missions/fetchissions',
+  async () => {
+    try {
+      const { data } = await axios.get(missionApiUrl);
+      return data;
+    } catch (err) {
+      return err.message;
+    }
+  },
+);
+
+const missionSlice = createSlice({
+  name: 'missions',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchMissions.pending]: (state) => {
+      state.status = true;
+    },
+    [fetchMissions.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.ifSucceed = true;
+      state.missions = action.payload;
+    },
+    [fetchMissions.rejected]: (state) => {
+      state.isLoading = false;
+    },
+  },
+});
+
+export { fetchMissions };
+
+export default missionSlice.reducer;
