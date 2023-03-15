b89c705514886dd3ba294ec4e2734bb08cd35796
Fithamlak <justiceofgod01@gmail.com>
2023-03-15 22:42:35 +0300

    Dispatch these actions upon click on the corresponding buttons.

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,18 +1,34 @@
 import PropTypes from 'prop-types';
 import './styles/mission.css';
+import { Button } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { joinMission, leaveMission } from '../redux/missions/missionsSlice';
 
-const Mission = ({ name, description }) => (
-  <div className="mission-container">
-    <ul className="mission">
-      <li><h3 className="name">{name}</h3></li>
-      <li><h2 className="title">{description}</h2></li>
-      <span>not a member</span>
-      <button type="button">join mission</button>
-    </ul>
-  </div>
-);
+const Mission = ({
+  id, name, description, joined,
+}) => {
+  const dispatch = useDispatch();
+  return (
+    <div className="mission-container">
+      <ul className="mission">
+        <li><h3 className="name">{name}</h3></li>
+        <li><h2 className="title">{description}</h2></li>
+        <span>Not active member</span>
+        <div>
+          {
+           joined
+             ? <Button variant="danger" onClick={() => dispatch(leaveMission(id))}>Leave Mission</Button>
+             : <Button variant="success" onClick={() => dispatch(joinMission(id))}>Join Mission</Button>
+          }
+        </div>
+      </ul>
+    </div>
+  );
+};
 Mission.propTypes = {
+  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  joined: PropTypes.bool.isRequired,
 };
 export default Mission;
