83ed4cd2677453eb45bb81ef6d6a15b603bada4e
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-15 22:33:11 +0430

    Add HandleRreserve button for the reservation

--- src/components/Rocket.jsx
+++ src/components/Rocket.jsx
@@ -1,10 +1,18 @@
 /* eslint-disable */
 import React from "react";
 import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
+import { setReserved } from "../redux/rockets/rocketsSlice";
 
 const Rocket = ({ id, name, type, img, description }) => {
+
+  const dispatch = useDispatch();
+  const handleReserve = (rocketId) => {
+    dispatch(setReserved(rocketId));
+  }
+
   return (
     <div className="d-flex p-3 gap-3">
       <img src={img} alt="Rocket" width="470" height="350"/>
@@ -12,7 +20,7 @@ const Rocket = ({ id, name, type, img, description }) => {
         <h2>{name}</h2>
         <p className="text-justify h5">{description}</p>
         <p className="h6">{type}</p>
-        <Button className="mt-4 py-2 px-4" varian="primary" id={id} >Reserve Rocket</Button>
+        <Button className="mt-4 py-2 px-4" varian="primary" onClick={() => handleReserve(id)} >Reserve Rocket</Button>
       </div>
     </div>
   );
