b2eb75ac94d496360288e10371cd15c2cabb6463
Fithamlak <justiceofgod01@gmail.com>
2023-03-16 00:15:09 +0300

    Create Myprofile route

---
+++ src/routes/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useSelector } from 'react-redux';
+
+const Profile = () => {
+  const { missions } = useSelector((store) => store.missions);
+  const joinedMissions = missions.filter((mission) => mission.joined);
+  return (
+    <div className="d-flex">
+      <div className="w-50">
+        <h2>My Missions</h2>
+        <JoinedMission mission={missions} joinedMissions={joinedMissions} />
+      </div>
+      <div className="w-50">
+        <h2>My Rockets</h2>
+      </div>
+    </div>
+  );
+};
+export default Profile;
