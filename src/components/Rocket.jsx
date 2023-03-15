1fd8acd0bc116bc9661c23380569558d50f90bb5
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-15 19:13:04 +0430

    Add style for each rocket container

--- src/components/Rocket.jsx
+++ src/components/Rocket.jsx
@@ -1,17 +1,22 @@
 /* eslint-disable */
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
-const Rocket = ({
-  id, name, type, img,
-}) => (
-  <div>
-    <h1>{name}</h1>
-    <h2>{type}</h2>
-    <h2>{id}</h2>
-    <img src={img} alt="Rocket" />
-  </div>
-);
+const Rocket = ({ id, name, type, img, description }) => {
+  return (
+    <div className="d-flex p-3 gap-3">
+      <img src={img} alt="Rocket" width="470" height="350"/>
+      <div className="pt-1">
+        <h2>{name}</h2>
+        <p className="text-justify h5">{description}</p>
+        <p className="h6">{type}</p>
+        <Button className="mt-4 py-2 px-4" varian="primary" id={id} >Reserve Rocket</Button>
+      </div>
+    </div>
+  );
+};
 
 Rocket.propTypes = {
   id: PropTypes.string.isRequired,
